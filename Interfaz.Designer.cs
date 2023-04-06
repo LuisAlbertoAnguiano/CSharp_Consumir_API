5191b70ebdd95c043f8d43ee07fee8e7b2084265
Asren <Asren@DESKTOP-NPTM86D>
2023-04-06 02:22:11 -0600

    Add project files.

---
+++ Interfaz.Designer.cs
@@ -0,0 +1,343 @@
+﻿namespace CSharp_Consumir_API
+{
+    partial class Interfaz
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TBX_CP = new System.Windows.Forms.TextBox();
+            this.TBX_Longitud = new System.Windows.Forms.TextBox();
+            this.TBX_Moneda = new System.Windows.Forms.TextBox();
+            this.TBX_Ciudad = new System.Windows.Forms.TextBox();
+            this.TBX_Region = new System.Windows.Forms.TextBox();
+            this.TBX_Pais = new System.Windows.Forms.TextBox();
+            this.TBX_Continente = new System.Windows.Forms.TextBox();
+            this.TBX_Latitud = new System.Windows.Forms.TextBox();
+            this.TBX_Version = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TBX_IP = new System.Windows.Forms.TextBox();
+            this.BT_Buscar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.Blue;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.White;
+            this.label10.Location = new System.Drawing.Point(542, 147);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(90, 25);
+            this.label10.TabIndex = 44;
+            this.label10.Text = "Moneda";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Blue;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(269, 247);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(80, 25);
+            this.label9.TabIndex = 43;
+            this.label9.Text = "Ciudad";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Blue;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(269, 198);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(80, 25);
+            this.label8.TabIndex = 42;
+            this.label8.Text = "Región";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Blue;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(228, 90);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(116, 25);
+            this.label7.TabIndex = 41;
+            this.label7.Text = "Continente";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Blue;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(555, 37);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 25);
+            this.label6.TabIndex = 40;
+            this.label6.Text = "Latitud";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Blue;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(537, 92);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 25);
+            this.label5.TabIndex = 39;
+            this.label5.Text = "Longitud";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Blue;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(303, 300);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 25);
+            this.label4.TabIndex = 38;
+            this.label4.Text = "CP";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Blue;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(295, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 25);
+            this.label3.TabIndex = 37;
+            this.label3.Text = "País";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Blue;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(264, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 25);
+            this.label2.TabIndex = 36;
+            this.label2.Text = "Versión";
+            // 
+            // TBX_CP
+            // 
+            this.TBX_CP.BackColor = System.Drawing.Color.White;
+            this.TBX_CP.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TBX_CP.Location = new System.Drawing.Point(355, 296);
+            this.TBX_CP.Name = "TBX_CP";
+            this.TBX_CP.ReadOnly = true;
+            this.TBX_CP.Size = new System.Drawing.Size(134, 29);
+            this.TBX_CP.TabIndex = 35;
+            // 
+            // TBX_Longitud
+            // 
+            this.TBX_Longitud.BackColor = System.Drawing.Color.White;
+            this.TBX_Longitud.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TBX_Longitud.Location = new System.Drawing.Point(638, 90);
+            this.TBX_Longitud.Name = "TBX_Longitud";
+            this.TBX_Longitud.ReadOnly = true;
+            this.TBX_Longitud.Size = new System.Drawing.Size(134, 29);
+            this.TBX_Longitud.TabIndex = 34;
+            // 
+            // TBX_Moneda
+            // 
+            this.TBX_Moneda.BackColor = System.Drawing.Color.White;
+            this.TBX_Moneda.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TBX_Moneda.Location = new System.Drawing.Point(638, 143);
+            this.TBX_Moneda.Name = "TBX_Moneda";
+            this.TBX_Moneda.ReadOnly = true;
+            this.TBX_Moneda.Size = new System.Drawing.Size(134, 29);
+            this.TBX_Moneda.TabIndex = 33;
+            // 
+            // TBX_Ciudad
+            // 
+            this.TBX_Ciudad.BackColor = System.Drawing.Color.White;
+            this.TBX_Ciudad.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TBX_Ciudad.Location = new System.Drawing.Point(355, 245);
+            this.TBX_Ciudad.Name = "TBX_Ciudad";
+            this.TBX_Ciudad.ReadOnly = true;
+            this.TBX_Ciudad.Size = new System.Drawing.Size(134, 29);
+            this.TBX_Ciudad.TabIndex = 32;
+            // 
+            // TBX_Region
+            // 
+            this.TBX_Region.BackColor = System.Drawing.Color.White;
+            this.TBX_Region.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TBX_Region.Location = new System.Drawing.Point(355, 194);
+            this.TBX_Region.Name = "TBX_Region";
+            this.TBX_Region.ReadOnly = true;
+            this.TBX_Region.Size = new System.Drawing.Size(134, 29);
+            this.TBX_Region.TabIndex = 31;
+            // 
+            // TBX_Pais
+            // 
+            this.TBX_Pais.BackColor = System.Drawing.Color.White;
+            this.TBX_Pais.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TBX_Pais.Location = new System.Drawing.Point(355, 143);
+            this.TBX_Pais.Name = "TBX_Pais";
+            this.TBX_Pais.ReadOnly = true;
+            this.TBX_Pais.Size = new System.Drawing.Size(134, 29);
+            this.TBX_Pais.TabIndex = 30;
+            // 
+            // TBX_Continente
+            // 
+            this.TBX_Continente.BackColor = System.Drawing.Color.White;
+            this.TBX_Continente.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TBX_Continente.Location = new System.Drawing.Point(355, 88);
+            this.TBX_Continente.Name = "TBX_Continente";
+            this.TBX_Continente.ReadOnly = true;
+            this.TBX_Continente.Size = new System.Drawing.Size(134, 29);
+            this.TBX_Continente.TabIndex = 29;
+            // 
+            // TBX_Latitud
+            // 
+            this.TBX_Latitud.BackColor = System.Drawing.Color.White;
+            this.TBX_Latitud.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TBX_Latitud.Location = new System.Drawing.Point(638, 35);
+            this.TBX_Latitud.Name = "TBX_Latitud";
+            this.TBX_Latitud.ReadOnly = true;
+            this.TBX_Latitud.Size = new System.Drawing.Size(134, 29);
+            this.TBX_Latitud.TabIndex = 28;
+            // 
+            // TBX_Version
+            // 
+            this.TBX_Version.BackColor = System.Drawing.Color.White;
+            this.TBX_Version.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TBX_Version.Location = new System.Drawing.Point(355, 35);
+            this.TBX_Version.Name = "TBX_Version";
+            this.TBX_Version.ReadOnly = true;
+            this.TBX_Version.Size = new System.Drawing.Size(134, 29);
+            this.TBX_Version.TabIndex = 27;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Blue;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 25);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "Direccion IP:";
+            // 
+            // TBX_IP
+            // 
+            this.TBX_IP.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TBX_IP.Location = new System.Drawing.Point(12, 129);
+            this.TBX_IP.Name = "TBX_IP";
+            this.TBX_IP.Size = new System.Drawing.Size(162, 29);
+            this.TBX_IP.TabIndex = 25;
+            // 
+            // BT_Buscar
+            // 
+            this.BT_Buscar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.BT_Buscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BT_Buscar.Location = new System.Drawing.Point(12, 197);
+            this.BT_Buscar.Name = "BT_Buscar";
+            this.BT_Buscar.Size = new System.Drawing.Size(162, 103);
+            this.BT_Buscar.TabIndex = 24;
+            this.BT_Buscar.Text = "Buscar";
+            this.BT_Buscar.UseVisualStyleBackColor = false;
+            this.BT_Buscar.MouseClick += new System.Windows.Forms.MouseEventHandler(this.BT_Buscar_MouseClick);
+            // 
+            // Interfaz
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(784, 361);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TBX_CP);
+            this.Controls.Add(this.TBX_Longitud);
+            this.Controls.Add(this.TBX_Moneda);
+            this.Controls.Add(this.TBX_Ciudad);
+            this.Controls.Add(this.TBX_Region);
+            this.Controls.Add(this.TBX_Pais);
+            this.Controls.Add(this.TBX_Continente);
+            this.Controls.Add(this.TBX_Latitud);
+            this.Controls.Add(this.TBX_Version);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TBX_IP);
+            this.Controls.Add(this.BT_Buscar);
+            this.Name = "Interfaz";
+            this.Load += new System.EventHandler(this.Interfaz_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TBX_CP;
+        private System.Windows.Forms.TextBox TBX_Longitud;
+        private System.Windows.Forms.TextBox TBX_Moneda;
+        private System.Windows.Forms.TextBox TBX_Ciudad;
+        private System.Windows.Forms.TextBox TBX_Region;
+        private System.Windows.Forms.TextBox TBX_Pais;
+        private System.Windows.Forms.TextBox TBX_Continente;
+        private System.Windows.Forms.TextBox TBX_Latitud;
+        private System.Windows.Forms.TextBox TBX_Version;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TBX_IP;
+        private System.Windows.Forms.Button BT_Buscar;
+    }
+}
+
---
+++ Interfaz.cs
@@ -0,0 +1,73 @@
+﻿//Instalar el paquete Newtonsoft.Json
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp_Consumir_API
+{
+    public partial class Interfaz : Form
+    {
+        //Declaracion de variables
+        string url = "https://ipapi.co/";
+        string ip = string.Empty;
+        string formato = "/json/";
+        string respuesta = string.Empty;
+        string direccion = string.Empty;
+        public Interfaz()
+        {
+            InitializeComponent();
+        }
+
+        private void Interfaz_Load(object sender, EventArgs e)
+        {
+
+        }
+        public async Task<string> GetHTTP()
+        {
+            //Solicitar y obtener la respuesta de la API
+            HttpWebRequest Request = (HttpWebRequest)WebRequest.Create(direccion);
+
+            Request.UserAgent = "ipapi.co/#c-sharp-v1.03";
+            HttpWebResponse response = (HttpWebResponse)Request.GetResponse();
+
+            //Leer la respuesta
+            StreamReader sr = new StreamReader(response.GetResponseStream());
+            //Regresar la respuesta
+            return await sr.ReadToEndAsync();
+        }
+
+        private async void BT_Buscar_MouseClick(object sender, MouseEventArgs e)
+        {
+            try
+            {   //Comprobar que la textbox de IP contenga datos
+                if (TBX_IP.Text != string.Empty)
+                {
+                    ip = TBX_IP.Text;
+                    direccion = url + ip + formato;
+                    respuesta = await GetHTTP();
+
+                    //Deserializar el archivo json
+                    var j = JsonConvert.DeserializeObject<Atributos>(respuesta);
+
+                    //Rellenar los textbox con la informacion deserializada del archivo json
+                    TBX_Version.Text = j.version;
+                    TBX_Continente.Text = j.continent_code;
+                    TBX_Pais.Text = j.country_name;
+                    TBX_Region.Text = j.region;
+                    TBX_Ciudad.Text = j.city;
+                    TBX_CP.Text = j.postal;
+                    TBX_Latitud.Text = j.latitude;
+                    TBX_Longitud.Text = j.longitude;
+                    TBX_Moneda.Text = j.currency_name;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("IP no valida");
+            }
+        }
+    }
+}
